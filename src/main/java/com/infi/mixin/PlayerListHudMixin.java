feff80e51f062e5af1448b1d49c0158fd2b1383a
Infi <105563974+infinitumcraft@users.noreply.github.com>
2026-06-09 10:08:32 +0200

    Ported to 1.21.11

--- src/main/java/com/infi/mixin/PlayerListHudMixin.java
+++ src/main/java/com/infi/mixin/PlayerListHudMixin.java
@@ -1,21 +1,21 @@
 package com.infi.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.client.gui.components.PlayerTabOverlay;
+import net.minecraft.client.gui.hud.PlayerListHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(PlayerTabOverlay.class)
+@Mixin(PlayerListHud.class)
 public class PlayerListHudMixin {
 	/*
 	Yes.
 	We just return true here, because minecraft checks server encrypting before rendering heads.
 	 */
 	@ModifyExpressionValue(
-			method = "extractRenderState",
+			method = "render",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/network/Connection;isEncrypted()Z"
+					target = "Lnet/minecraft/network/ClientConnection;isEncrypted()Z"
 			)
 	)
 	private boolean onTabRender(boolean original) {
